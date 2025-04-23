58f7e304b3936b0415c8dc95371cb703c39fa889
mezz01 <mezzatamine2002@gmail.com>
2025-04-23 15:13:15 +0100

    public for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://plombiertest.com",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap(
+  {
+    changefreq: "weekly",
+      priority: 0.9,
+      lastmod: new Date(),
+  }
+  )]
+});
