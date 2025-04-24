e66738f9e41f76272d3b334df40a519218550493
mezz01 <mezzatamine2002@gmail.com>
2025-04-25 00:35:01 +0100

    homepage convertion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon"; 
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +18,7 @@ export default defineConfig({
       priority: 0.9,
       lastmod: new Date(),
   }
-  )]
+  ),
+  icon()
+]
 });
