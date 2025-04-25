de5043a535725499864ee4f758c069274b1093a5
mezz01 <mezzatamine2002@gmail.com>
2025-04-25 18:21:10 +0100

    urls and programmatic

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -3751,3 +3751,16 @@ export const data = [
     ],
   },
 ];
+
+
+export function slugify(text: string): string {
+  return text
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[\s-]+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .trim();
+}
