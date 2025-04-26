f4c634164179bed9a68b54691b7710ea10ec5982
mezz01 <mezzatamine2002@gmail.com>
2025-04-26 11:53:10 +0100

    programmatic nd seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://plombiertest.com",
+  site: "https://plombier.be",
   vite: {
     plugins: [tailwindcss()]
   },
